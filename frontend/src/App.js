6aefc388346d0dd4800c2df8734aaf324275cd01
aggelooskk <akermezo@yahoo.com>
2024-06-01 11:08:24 +0300

    first commit

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "./Components/Header";
+
+function App() {
+  return (
+    <>
+    <Header />
+      <h1>Hello World</h1>
+    
+    </>
+  );
+}
+
+export default App;
